b20b991183415f40bd8a587ea0bdfe920f7553f7
Fire-monsters <kalibobojoshua@gmail.com>
2026-05-28 13:57:05 +0300

    Initial commit

--- core/App.tsx
+++ core/App.tsx
@@ -1,30 +1,33 @@
 import React from 'react';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
 import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
 import { store } from '../src/store';
-import { NewAppScreen } from '@react-native/new-app-screen';
 
 function AppContent() {
   const safeAreaInsets = useSafeAreaInsets();
   return (
-    <View style={styles.container}>
-      <NewAppScreen templateFileName="App.tsx" safeAreaInsets={safeAreaInsets} />
+    <View style={[styles.container, { paddingTop: safeAreaInsets.top }]}>
+      <Text style={styles.text}>Welcome to Ekatale Logistics</Text>
     </View>
   );
 }
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <Provider store={store}>
-      <SafeAreaProvider>
-        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-        <AppContent />
-      </SafeAreaProvider>
-    </Provider>
+  const appContent = (
+    <SafeAreaProvider>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <AppContent />
+    </SafeAreaProvider>
   );
+
+  return <Provider store={store} children={appContent} />;
 }
 
-const styles = StyleSheet.create({ container: { flex: 1 } });
+const styles = StyleSheet.create({ 
+  container: { flex: 1 }, 
+  text: { fontSize: 18, 
+    fontWeight: 'bold' } 
+});
 export default App;
